6c6d66a03a01c5498ee8bd3349cafc78d1c1d456
Arti <tienduangmaiphan1998@gmail.com>
2023-05-09 09:36:45 +0700

    remove some unused function

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
 from flask import Flask, jsonify, render_template, request
-from flask_restful import Resource, Api, reqparse
+from flask_restful import Resource, Api
 from utils import tts_fptAI, play_mp3
+
+
 app = Flask(__name__)
 api = Api(app)
 
-output = {}
 
 # def Model_init():
 #     # import model from the model file can be pretrained or fine tuned
@@ -12,7 +13,7 @@ output = {}
 #     blender_agent = create_agent_from_model_file(
 #         "zoo:blender/blender_90M/model")
 #     return blender_agent
-
+# Load speech data from fpt server
 url = 'https://api.fpt.ai/hmi/tts/v5'
 payload = ''
 headers = {
@@ -24,10 +25,10 @@ headers = {
 @app.route("/")
 def home():
     return render_template("index.html")
+
 @app.route('/welcome', methods=["POST"])
 def voice_welcome():
     welcome = "Xin chào, tôi là trợ lý ảo Ban công tác xã hội của câu lạc bộ Doanh nhân Sài Gòn, tôi có thẻ giúp gì cho bạn?"
-    # SpeakText(welcome)
     audio = tts_fptAI(welcome)
     play_mp3("data/audio/" + audio)
     return jsonify(welcome)
--- utils.py
+++ utils.py
@@ -1,29 +1,29 @@
-import sqlite3
+# import sqlite3
 import requests
-import os
-from gtts import gTTS
-from playsound import playsound
+# import os
+# from gtts import gTTS
+# from playsound import playsound
 import time
 import wget
 import subprocess
-import pandas as pd
+# import pandas as pd
 
 # src tu bot - vi tu user - en
-def transText(text_input, scr_input='bot'):
-    from googletrans import Translator
-    # define a translate object
-    translate = Translator()
-    # Translate some text
-    # scr_input = translate.detect(text_input)
-    if scr_input == "bot":
-        result = translate.translate(text_input, src='en', dest='vi')
-        result = result.text
-    elif scr_input == "user":
-        result = translate.translate(text_input, src='vi', dest='en')
-        result = result.text
-    else:
-        result = "We not support this language, please use English or Vietnamese!"
-    return result
+# def transText(text_input, scr_input='bot'):
+#     from googletrans import Translator
+#     # define a translate object
+#     translate = Translator()
+#     # Translate some text
+#     # scr_input = translate.detect(text_input)
+#     if scr_input == "bot":
+#         result = translate.translate(text_input, src='en', dest='vi')
+#         result = result.text
+#     elif scr_input == "user":
+#         result = translate.translate(text_input, src='vi', dest='en')
+#         result = result.text
+#     else:
+#         result = "We not support this language, please use English or Vietnamese!"
+#     return result
 
 # Write error to txt
 
@@ -36,24 +36,6 @@ def save_error(message, error_source):
     f.write('\n')
     # closing the file
     f.close()
-
-def SpeakText(command, lang_input='vi'):
-    # Passing the text and language to the engine,
-    # here we have marked slow=False. Which tells
-    # the module that the converted audio should
-    # have a high speed
-    myobj = gTTS(text=command, lang=lang_input, slow=False)
-
-    # Saving the converted audio in a mp3 file named
-    # welcome
-    myobj.save("temp.mp3")
-    playsound('temp.mp3')
-    # Playing the converted file
-    # os.system("mpg123 temp.mp3")
-
-
-def train_rb_model():
-    pass
 
 # Text to Speech using FPT_AI api
 def tts_fptAI(msg):
@@ -89,22 +71,21 @@ def tts_fptAI(msg):
 def play_mp3(path):
     subprocess.Popen(['mpg123', '-q', path]).wait()
 
-def event_response(tag):
-    con = sqlite3.connect("apps/db.sqlite3")
-    df = pd.read_sql_query("SELECT * from Events", con)
-    df = df.drop(['oauth_github'], axis=1)
-    if tag == "Golf_session_01":
-        res = ["Vâng, tuần này CLB có tổ chức môt buổi " + df.eventName.values[-1], "act01/Golf_session_01"]
-    elif tag == "Golf_session_02":
-        res = ["Buổi chơi golf sẽ diễn ra tại " + df.address.values[-1] + " vào lúc " + df.dateTime.values[-1], "act01/Golf_session_02"]
-    elif tag == "Golf_session_03":
-        res = ["Buổi chơi golf nhằm " + df.content.values[-1], "act01/Golf_session_03"]
-    else:
-        res = "None"
+# def event_response(tag):
+#     con = sqlite3.connect("apps/db.sqlite3")
+#     df = pd.read_sql_query("SELECT * from Events", con)
+#     df = df.drop(['oauth_github'], axis=1)
+#     if tag == "Golf_session_01":
+#         res = ["Vâng, tuần này CLB có tổ chức môt buổi " + df.eventName.values[-1], "act01/Golf_session_01"]
+#     elif tag == "Golf_session_02":
+#         res = ["Buổi chơi golf sẽ diễn ra tại " + df.address.values[-1] + " vào lúc " + df.dateTime.values[-1], "act01/Golf_session_02"]
+#     elif tag == "Golf_session_03":
+#         res = ["Buổi chơi golf nhằm " + df.content.values[-1], "act01/Golf_session_03"]
+#     else:
+#         res = "None"
 
-    return res
+#     return res
 
 if __name__ == "__main__":
     print("Test function")
-    print(transText("Phượng Hoàng Lửa rơi mặt nạ, Lady Mây tung hit mới"))
-    SpeakText("Phượng Hoàng Lửa rơi mặt nạ, Lady Mây tung hit mới")
+
