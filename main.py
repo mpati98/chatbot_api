3e97580e7e8e78c719e9d5cb52e731a2d289f9cc
Arti <tienduangmaiphan1998@gmail.com>
2023-05-10 09:40:16 +0700

    add rule-based chatbot

--- main.py
+++ main.py
@@ -1,26 +1,93 @@
 from flask import Flask, jsonify, render_template, request
 from flask_restful import Resource, Api
-# from utils import tts_fptAI, play_mp3
+import nltk
+nltk.download('popular')
+from nltk.stem import WordNetLemmatizer
+lemmatizer = WordNetLemmatizer()
+import pickle
+import numpy as np
+from keras.models import Sequential
+from keras.layers import Dense, Activation, Dropout
+from keras.optimizers import SGD
+
+# Load data
+from keras.models import load_model
+model = load_model('model.h5')
+import json
+import random
+intents = json.loads(open('data/data.json').read())
+words = pickle.load(open('texts.pkl','rb'))
+classes = pickle.load(open('labels.pkl','rb'))
+
+def transText(text_input, scr_input='user'):
+    from googletrans import Translator
+    # define a translate object
+    translate = Translator()
+    if scr_input == "bot":
+        result = translate.translate(text_input, src='en', dest='vi')
+        result = result.text
+    elif scr_input == "user":
+        result = translate.translate(text_input, src='vi', dest='en')
+        result = result.text
+    else:
+        result = "We not support this language, please use English or Vietnamese!"
+    return result
+
+def clean_up_sentence(sentence):
+    # tokenize the pattern - split words into array
+    sentence_words = nltk.word_tokenize(sentence)
+    # stem each word - create short form for word
+    sentence_words = [lemmatizer.lemmatize(word.lower()) for word in sentence_words]
+    return sentence_words
+
+# return bag of words array: 0 or 1 for each word in the bag that exists in the sentence
+
+def bow(sentence, words, show_details=True):
+    # tokenize the pattern
+    sentence_words = clean_up_sentence(sentence)
+    # bag of words - matrix of N words, vocabulary matrix
+    bag = [0]*len(words)  
+    for s in sentence_words:
+        for i,w in enumerate(words):
+            if w == s: 
+                # assign 1 if current word is in the vocabulary position
+                bag[i] = 1
+                if show_details:
+                    print ("found in bag: %s" % w)
+    return(np.array(bag))
+
+def predict_class(sentence, model):
+    # filter out predictions below a threshold
+    p = bow(sentence, words,show_details=False)
+    res = model.predict(np.array([p]))[0]
+    ERROR_THRESHOLD = 0.25
+    results = [[i,r] for i,r in enumerate(res) if r>ERROR_THRESHOLD]
+    # sort by strength of probability
+    results.sort(key=lambda x: x[1], reverse=True)
+    return_list = []
+    for r in results:
+        return_list.append({"intent": classes[r[0]], "probability": str(r[1])})
+    return return_list
+
+def getResponse(ints, intents_json):
+    tag = ints[0]['intent']
+    list_of_intents = intents_json['intents']
+    for i in list_of_intents:
+        if(i['tag']== tag):
+            result = random.choice(i['responses'])
+            break
+    return result
+
+def chatbot_response(msg):
+    ints = predict_class(msg, model)
+    res = getResponse(ints, intents)
+    return res
 
 
 app = Flask(__name__)
 api = Api(app)
 
 
-# def Model_init():
-#     # import model from the model file can be pretrained or fine tuned
-
-#     blender_agent = create_agent_from_model_file(
-#         "zoo:blender/blender_90M/model")
-#     return blender_agent
-# Load speech data from fpt server
-url = 'https://api.fpt.ai/hmi/tts/v5'
-payload = ''
-headers = {
-    'api-key': 'OaGgkRmCMzOoZbnwgX70IDWo77t0g5SW',
-    'speed': '',
-    'voice': 'banmai'
-}
 
 @app.route("/")
 def home():
@@ -29,8 +96,6 @@ def home():
 @app.route('/welcome', methods=["POST"])
 def voice_welcome():
     welcome = "Xin chào, tôi là trợ lý ảo Ban công tác xã hội của câu lạc bộ Doanh nhân Sài Gòn, tôi có thẻ giúp gì cho bạn?"
-    # audio = tts_fptAI(welcome)
-    # play_mp3("data/audio/default/introduce.mp3")
     res = {
         "msg": welcome,
         "audio": "data/audio/default/introduce.mp3"
@@ -41,24 +106,12 @@ def voice_welcome():
 class Chatbot(Resource):
 
     def post(self):
-        # Use parser and find the user's query
-        # args = parser.parse_args()
-        # sentence = args['q']
+
         text_input = request.get_json().get("message")
-        # try:
-        #     print(text_input)
-        #     re
-        # except:
-        #     res = [
-        #             "Bạn có thể liên hệ Miss Phương Trinh là Thư ký CLB qua số 0388 372 691 để biết thêm về thông tin này",
-        #             # "default/golfActivitiesInfor04"
-        #         ]
-        #         # tts_fptAI(res)
-        #         # SpeakText(res)
-        #     res_text = res[0]
-        #     # res_audio = res[1]
+        text_input = transText(text_input)
+        resp = chatbot_response(text_input)
         output = {
-            "res_text": text_input,
+            "res_text": resp,
             # "res_audio": res_audio
         }
         return jsonify(output)
